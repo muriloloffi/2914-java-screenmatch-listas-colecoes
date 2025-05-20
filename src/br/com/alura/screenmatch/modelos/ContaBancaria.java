e55cb421b2922f4992089be94a267a5a876745da
Murilo Loffi <mu110655@casan.com.br>
2025-05-20 11:30:37 -0300

    Continuação lição comparable

--- src/br/com/alura/screenmatch/modelos/ContaBancaria.java
+++ src/br/com/alura/screenmatch/modelos/ContaBancaria.java
@@ -16,11 +16,23 @@ public class ContaBancaria implements Comparable<ContaBancaria> {
     }
 
     public String toString() {
-        return "%s, saldo: %.2f".formatted(this.numeroConta, this.getSaldo().doubleValue());
+        return "%s, saldo: %.2f".formatted(
+                this.numeroConta,
+                this.getSaldo().doubleValue());
     }
 
     @Override
     public int compareTo(ContaBancaria outraConta) {
         return this.getSaldo().compareTo(outraConta.getSaldo());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
